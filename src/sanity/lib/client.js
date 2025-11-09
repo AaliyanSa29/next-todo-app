f9df5973fc91890cc09e647599c7174bd01d3e40
AaliyanSami29 <aaliyan.sami804@gmail.com>
2025-11-09 18:33:31 +0500

    Fix Sanity API version

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: "2025-11-01",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "f3aif6e4",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: "2024-11-09", // ✅ Changed from 2025-11-01
   useCdn: true,
 });
